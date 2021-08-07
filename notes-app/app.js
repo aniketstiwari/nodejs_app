c625100d88cf747b6a967c0c9d09719f865b5900
aniketstiwari <anikaittiwari91@gmail.com>
2021-08-07 16:34:29 +0530

    [16] Arguments passing with Yargs part 1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+
 //const fs = require('fs')
 
 //fs.writeFileSync("notes.txt", 'this file was created by node js')
@@ -24,7 +27,6 @@
 //console.log(validator.isEmail('foo@bar.com'));
 //console.log(validator.isURL('asdadfoo@b'));
 
-// const chalk = require('chalk');
 // console.log(chalk.bold.green.inverse('Success'));
 
 //Pass arguments from command line
@@ -32,10 +34,63 @@
 //node app.js add --title="THis is my title"
 
 //console.log(process.argv)
-const command = process.argv[2]
+//const command = process.argv[2]
 //console.log(process.argv)
-if(command === 'add') {
-  console.log('Adding notes')
-}else if(command === 'remove') {
-    console.log("removing notes")
-}
+// if(command === 'add') {
+//   console.log('Adding notes')
+// }else if(command === 'remove') {
+//     console.log("removing notes")
+// }
+
+//console.log(process.argv)
+//console.log(yargs.argv)
+
+//node app.js  --help It will give some properties of yargs
+//To get version of yargs node app.js   --version
+
+//customizing yargs version
+yargs.version('1.1.0')
+
+//create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log("adding a new note")
+    }
+})
+
+//console.log(yargs.argv)
+// node app.js add (when calling from terminal it will call the handler method)
+
+//create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function() {
+        console.log("removing a new note")
+    }
+})
+
+//node app.js --help it will both the command added 'add' and 'remove'
+//console.log(yargs.argv)
+
+yargs.command({
+    command: "list",
+    describe: "List your notes",
+    handler: function() {
+        console.log("Listing out all notes")
+    }  
+})
+
+yargs.command({
+    command: "read",
+    describe: "Read a notes",
+    handler: function() {
+        console.log("Reading a note")
+    }  
+})
+
+console.log(yargs.argv)
