44aaae6d12eb53237d36d75ffb702df5283d1c4c
aniketstiwari <anikaittiwari91@gmail.com>
2021-08-07 14:20:53 +0530

    notes app

---
+++ notes-app/app.js
@@ -0,0 +1,25 @@
+//const fs = require('fs')
+
+//fs.writeFileSync("notes.txt", 'this file was created by node js')
+
+//fs.appendFileSync('message.txt', ' data to append ssss');
+
+//const name = require('./utils.js')
+
+//const name = 'Aniket'
+
+//console.log(name);
+
+// const add = require('./utils.js')
+
+// const sum = add(4,-2);
+// console.log(sum);
+
+// const getNotes = require('./notes.js')
+
+// console.log(getNotes())
+
+const validator = require('validator')
+
+//console.log(validator.isEmail('foo@bar.com'));
+console.log(validator.isURL('asdadfoo@b'));
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log("util.js");
+
+const name = 'SHivam';
+
+const add = function(x, y) {
+    return x + y;
+}
+
+module.exports = add;
