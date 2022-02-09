028a2f018933bdf02647896b7f70735adb85e03e
aniketstiwari <anikaittiwari91@gmail.com>
2022-02-09 21:43:28 +0530

    [31] Making http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,19 @@
-console.log("starting")
+// console.log("starting")
 
-setTimeout(() => {
-  console.log('2 second timer') 
-}, 2000)
+// setTimeout(() => {
+//   console.log('2 second timer') 
+// }, 2000)
 
-setTimeout(() => {
- console.log('0 second timer')
-}, 0)
+// setTimeout(() => {
+//  console.log('0 second timer')
+// }, 0)
 
-console.log("Stopping")
+// console.log("Stopping")
+
+const request = require('request')
+const url = 'http://api.weatherstack.com/current?access_key=ee6c9ddd2a5760209bbb6d19553ff7b6&query=37.8267,-122.4233'
+
+request({ url: url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
