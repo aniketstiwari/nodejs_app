e61f2bf54bc0878406156219d4b07258d8e50fbf
aniketstiwari <anikaittiwari91@gmail.com>
2022-02-11 10:49:34 +0530

    [32] Customizing HTTP requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -11,9 +11,11 @@
 // console.log("Stopping")
 
 const request = require('request')
-const url = 'http://api.weatherstack.com/current?access_key=ee6c9ddd2a5760209bbb6d19553ff7b6&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=ee6c9ddd2a5760209bbb6d19553ff7b6&query=37.8267,-122.4233&units=f'
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true}, (error, response) => {
+    //const data = JSON.parse(response.body)
+    //console.log(data.current)
+    const responseBody = response.body.current
+    console.log(responseBody.weather_descriptions[0] + ". It is currently " + responseBody.temperature + " degrees out. It feels like " + responseBody.feelslike  + " degress out.")
 })
